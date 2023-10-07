c973233e00f32a062bb80b7f8db58afef83f38f4
prabhavpokharel <akacaptainfury@gmail.com>
2023-10-07 21:57:42 +0545

    Add a Homepage Contents

--- main.c
+++ main.c
@@ -2,7 +2,23 @@
 
 void main()
 {
+	int input;
 	
+	printf("\t  C-BANK PRO");
+	printf("\n\"Experience Banking, the PRO way\"");
+	printf("\n==================================");
+		
+	printf("\n\nHomepage");
+	printf("\n\n[1] Login");
+	printf("\n[2] Create an Account");
+	printf("\n[3] Deposit Cash");
+	printf("\n[4] Withdraw Cash");
+	printf("\n[5] Balance Inquiry");
+	printf("\n[6] Transaction History");
+	printf("\n[7] Customer Support");
+	
+	printf("\n\nEnter your choice: ");
+	scanf("%d", &input);
 	
 	getch();
 }
