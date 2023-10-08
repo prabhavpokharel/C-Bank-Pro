f748348005272de33e4e00bb3a6c99d337e9f1ab
prabhavpokharel <akacaptainfury@gmail.com>
2023-10-08 10:10:55 +0545

    Modify homepage and customer support page

--- main.c
+++ main.c
@@ -1,12 +1,22 @@
 #include<stdio.h>
+#include<conio.h>
+
+void homepage();
+void csupport();
 
 void main()
 {
+	homepage();
+}
+
+void homepage()
+{
 	int input;
 	
+	system("cls");
 	printf("\t  C-BANK PRO");
 	printf("\n\"Experience Banking, the PRO way\"");
-	printf("\n==================================");
+	printf("\n=================================");
 		
 	printf("\n\nHomepage");
 	printf("\n\n[1] Login");
@@ -16,10 +26,73 @@ void main()
 	printf("\n[5] Balance Inquiry");
 	printf("\n[6] Transaction History");
 	printf("\n[7] Customer Support");
+	printf("\n[8] Exit");	
 	
 	printf("\n\nEnter your choice: ");
-	scanf("%d", &input);
+	fflush(stdin);
+	input = getch();
 	
-	getch();
+	switch(input)
+	{
+		case '1':
+			break;
+			
+		case '2':
+			break;
+			
+		case '3':
+			break;
+			
+		case '4':
+			break;
+			
+		case '5':
+			break;
+		
+		case '6':
+			break;
+			
+		case '7':
+			csupport();
+			break;
+			
+		case '8':
+			printf("\nThanks for trusting C-Bank Pro. Safe Banking, Safe Life!");
+			getch();
+			break;
+			
+		default:
+			printf("\nError: Option not available");
+	}
 }
+
+void csupport()
+{
+	char home;
+	
+	system("cls");
+	printf("\t  C-BANK PRO");
+	printf("\n\"Experience Banking, the PRO way\"");
+	printf("\n=================================");
+		
+	printf("\n\nCustomer Support");
+	printf("\nAvailable 24/7 in 365 Days");
+	printf("\n\nHotline: +977-1-2345678");
+	printf("\nToll-Free: 1660-1-2345678");
+	printf("\nPhone: +977-9845685245");
+	
+	printf("\n\nEnter H to go to HOMEPAGE: ");
+	fflush(stdin);
+	home = getch();
+	
+	if(home =='h' || home == 'H')
+	{
+		homepage();
+	}else
+	{
+		printf("Invalid Input");
+		getch();
+	}
+}
+
 
