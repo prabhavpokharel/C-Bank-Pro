0dbb3bc4dd93045a01b75e4dd2fd4a28a311c4a6
Prabhav Pokharel <akacaptainfury@gmail.com>
2023-10-09 14:21:37 +0545

    Update main.c

--- main.c
+++ main.c
@@ -2,7 +2,6 @@
 #include<conio.h>
 #include<string.h>
 #include<ctype.h>
-#include<stdlib.h>
 
 #define MAX_PASSWORD_LENGTH 30
 
@@ -18,9 +17,8 @@ void logpassword(struct account);
 void menu(struct account);
 void createacc();
 void balanceInq(struct account);
-void deposit(struct account *);
 void csupport(struct account);
-void exitMessage();
+void footer();
 
 void main()
 {
@@ -28,7 +26,7 @@ void main()
 	
 	header();
 	
-	printf("Homepage");
+	printf("\n\nHomepage");
 	printf("\n\n[1] Login");
 	printf("\n[2] Create an Account");
 	printf("\n[3] Exit");	
@@ -48,7 +46,7 @@ void main()
 			break;
 			
 		case '3':
-			exitMessage();
+			footer();
 			break;
 			
 		default:
@@ -171,11 +169,12 @@ void menu(struct account a)
 
 void createacc()
 {
+	system("cls");
 	struct account a;
 	
 	header();
 	
-	printf("Enter first name: ");
+	printf("\n\nEnter first name: ");
 	fflush(stdin);
 	scanf("%s",a.firstname);
 	
@@ -277,129 +276,18 @@ void logpassword(struct account a)
 		fclose(fp);
 		printf("\n\nRedirecting to login page...");
 		sleep(1);
-		
+		system("cls");
 		login();
-	}
-}
-
-void login()
-{
-	FILE *fptr;
-	
-	int j=0;
-	long long int phn;
-	char pw[MAX_PASSWORD_LENGTH], ch;
-	struct account a;
-
-	header();
-		
-	printf("Login");
-	printf("\n\nEnter your mobile number: +977 ");
-	fflush(stdin);
-	scanf("%lld", &phn);
-	printf("Enter password: ");
-	fflush(stdin);
-	while (1) 
-	{
-        ch = getch();
-        if (ch == 13)
-		{
-            break;
-        }
-		else if (ch == 8 || ch == 127)
-		{
-            if (j > 0)
-			{
-                j--;
-                printf("\b \b");
-            }
-        }
-		else if (!isspace(ch)) 
-		{
-            if (j < MAX_PASSWORD_LENGTH - 1)
-			{
-                putchar('*');
-                pw[j] = ch;
-                j++;
-            } 
-			else
-			{
-				
-            }
-        }
-    }
-    pw[j] = '\0';
-	
-	fptr = fopen("accounts.bin", "rb");
-	
-	while(fread(&a, sizeof(a), 1, fptr)!=0)
-	{
-		if((a.phone==phn) && (strcmp(a.password,pw)==0))
-		{
-			menu(a);
-		}
-		else
-		{
-			printf("\nUser not found");
-		}
-	}
-	
-	fclose(fptr);
-}
-
-void menu(struct account a)
-{
-	char input;
-	
-	header();
-		
-	printf("Welcome %s,", a.firstname);
-	printf("\n\n[1] Deposit Cash");
-	printf("\n[2] Withdraw Cash");
-	printf("\n[3] Balance Inquiry");
-	printf("\n[4] Transaction History");
-	printf("\n[5] Customer Support");
-	printf("\n[6] Exit");	
-	
-	printf("\n\nEnter your choice: ");
-	fflush(stdin);
-	input = getch();
-	
-	switch(input)
-	{
-		case '1':
-			deposit(&a);
-			break;
-			
-		case '2':
-			break;
-			
-		case '3':
-			balanceInq(a);
-			break;
-			
-		case '4':
-			break;
-			
-		case '5':
-			csupport(a);
-			break;			
-			
-		case '6':
-			exitMessage();
-			break;
-			
-		default:
-			printf("\nError: Option not available");
 	}
 }
 
 void balanceInq(struct account a)
 {
 	header();
+	
 	char input;
 	
-	printf("Available Balance: %.2lf", a.balance);
+	printf("\n\nAvailable Balance: %lf", a.balance);
 	
 	printf("\n\n[1] Back");
 	printf("\n[2] Exit");
@@ -412,35 +300,11 @@ void balanceInq(struct account a)
 		menu(a);
 	}else if(input == '2')
 	{
-		exitMessage();
+		footer();
 	}else
 	{
 		balanceInq(a);
 	}
-}
-
-void deposit(struct account *a)
-{
-	FILE *fptr;
-	header();
-	int amount;
-	
-	printf("Enter the amount you want to deposit: ");
-	scanf("%d", &amount);
-	
-	a->balance += (double)amount;
-	
-	fptr = fopen("accounts.bin", "rb+");
-
-    fseek(fptr, -sizeof(struct account), SEEK_CUR);
-
-    fwrite(a, sizeof(struct account), 1, fptr);
-    fclose(fptr);
-	
-	printf("Your amount has been successfully deposited!");
-	sleep(1);
-	
-	balanceInq(*a);
 }
 
 void csupport(struct account a)
@@ -449,7 +313,7 @@ void csupport(struct account a)
 	
 	header();
 		
-	printf("Customer Support - Available 24/7 in 365 Days");
+	printf("\n\nCustomer Support - Available 24/7 in 365 Days");
 	printf("\n\nHotline: +977-1-2345678");
 	printf("\nToll-Free: 1660-1-2345678");
 	printf("\nPhone: +977-9845685245");
@@ -466,7 +330,7 @@ void csupport(struct account a)
 		menu(a);
 	}else if(input == '2')
 	{
-		exitMessage();
+		footer();
 	}else
 	{
 		csupport(a);
@@ -478,15 +342,12 @@ void header()
 	system("cls");
 	printf("\t  C-BANK PRO");
 	printf("\n\"Experience Banking, the PRO way\"");
-	printf("\n=================================\n\n");
+	printf("\n=================================");
 }
 
-void exitMessage()
+void footer()
 {
-	header();
-	
+	system("cls");
 	printf("Thanks for trusting C-Bank Pro. Safe Banking, Safe Life.");
 	sleep(1);
-	exit(1);
 }
-
